4120a132cfa27aebf0b58be7fa6696f42cc05c1e
thatone111 <167234298+thatone111@users.noreply.github.com>
2024-04-29 12:26:30 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,12 @@
+namespace SpriteKind {
+    export const Eosinophils = SpriteKind.create()
+    export const Neutrophils = SpriteKind.create()
+    export const Monocytes = SpriteKind.create()
+    export const Basophils = SpriteKind.create()
+    export const Lymphocites = SpriteKind.create()
+    export const TCells = SpriteKind.create()
+    export const BCells = SpriteKind.create()
+    export const Antigen = SpriteKind.create()
+}
 game.splash("INTRODUCING THE BODIES", "FUNCTIONS")
 scene.setBackgroundImage(assets.image`Bob`)
